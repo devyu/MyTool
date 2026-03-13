6f2e84e1b736df8bec28a280bda628057cf8a267
jingyu <dev.jingyu@gmail.com>
2026-03-13 18:06:38 +0800

    init package

--- Package.swift
+++ Package.swift
@@ -1,6 +1,5 @@
 // swift-tools-version: 6.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
@@ -15,14 +14,20 @@ let package = Package(
             targets: ["MyTool"]
         ),
     ],
+    dependencies: [
+        .package(
+            url: "https://github.com/Alamofire/Alamofire.git",
+            from: "5.8.0"
+        )
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "MyTool",
-            resources: [
-                .process("Resources")
-            ],
+            dependencies: [
+                .product(name: "Alamofire", package: "Alamofire")
+            ]
         ),
 
     ]
